686d9add2464943d7421f5200df7d3a57fc8584f
Arwen Hawes <ahawes@unca.edu>
2013-02-11 08:39:02 -0500

    removed hello world

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var main = function(){ //MEMORIZE THIS!
-  console.log("calling main method");
+  
   $(".content").append("<p>Hello from jQuery</p>");//always start a jquery with  $ and inside the paranthesis choose selector
   $(".content").append("<p id='watchthis' style='display:none'>Watch This!</p>")
   
